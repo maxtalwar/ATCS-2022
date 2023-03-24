1884ae3b14fd10be2fefcd46bba358283708406c
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-03-24 13:51:48 -0700

    started work on models

--- twitter/models.py
+++ twitter/models.py
@@ -25,6 +25,9 @@ class User(Base):
                              primaryjoin="User.username==Follower.following_id",
                              secondaryjoin="User.username==Follower.follower_id",
                              overlaps="following")
+    
+    def __repr__(self):
+        return f"@{self.username}"
 
 
 class Follower(Base):
@@ -36,13 +39,35 @@ class Follower(Base):
     following_id = Column('following_id', TEXT, ForeignKey('users.username'))
 
 class Tweet(Base):
-    # TODO: Complete the class
-    pass
+    __tablename__ = "tweets"
 
-class Tag(Base):
-    # TODO: Complete the class
-    pass
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", TEXT, nullable=False)
+    timestamp = Column("timestamp", TEXT, nullable=False)
+    username = Column("username", TEXT, nullable=False)
+
+    # Relationships
+    tags = relationship("Tag", secondary="tweettags", back_populates="tweets")
+
+
+    def __repr__(self):
+        return f"{self.username}\n{self.content}\n{}"
 
 class TweetTag(Base):
-    # TODO: Complete the class
-    pass
+    __tablename__ = "tweettags"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    tweet_id = Column("tweet_id", INTEGER, ForeignKey("tweet.id"))
+    tag_id = Column("tag_id", INTEGER, ForeignKey("tag.id"))
+
+class Tag(Base):
+    __tablename__ = "tag"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", TEXT, nullable=False)
+
+    def __repr__(self):
+        return f"#{self.content}"
